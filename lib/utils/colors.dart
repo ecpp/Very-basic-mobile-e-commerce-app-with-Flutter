060ce10d9784432a1da2435d3bb6a1c8d2a245fa
yetkinsonmez <71431107+yetkinsonmez@users.noreply.github.com>
2021-11-29 20:29:45 +0300

    Update colors.dart

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -11,6 +11,7 @@ class AppColors {
   static const Color navColor = Colors.white;
   static const Color maintextColor = Colors.black;
   static const Color borderColor = Colors.white;
+  static const Color walkColor = Colors.orange;
 
   static const Color textColor = Colors.black;
   static const Color headingColor = const Color(0xFFD4F634);
